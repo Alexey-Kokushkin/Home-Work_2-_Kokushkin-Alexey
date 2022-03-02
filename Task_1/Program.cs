6ce513f3e0340d17f48c910c2cc926f8ba6b8bc4
Alexey-Kokushkin <kokushkin_83@mail.ru>
2022-03-02 21:51:23 +0900

    Task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите число: ");
+String inputnumber = Console.ReadLine();
+int number = int.Parse(inputnumber);
+
+int dif(int number)
+{
+    int dec = number / 10;
+    int units = dec % 10;
+    return units;
+}
+
+int result = dif(number); 
+Console.WriteLine(result);
+
+
+//int[]array = {number};
+//int n = array.Length;
+//Console.Write(n);
